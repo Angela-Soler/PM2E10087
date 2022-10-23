fb9ac70bd6a3166f35628f7cb93a88a2da506e44
Angela Soler <an_soler@hotmail.com>
2022-10-22 23:11:21 -0600

    Cambios en diseño de Activity Main

--- app/src/main/java/com/example/pm2e10087/MainActivity.java
+++ app/src/main/java/com/example/pm2e10087/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.pm2e10087;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaracion de variables
+    Spinner spPaises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Agregando valores al Spinner
+        spPaises = findViewById(R.id.spPaises);
+        String[] opciones = {"Honduras (504)", "Guatemala (502)","Costa Rica","Nicaragua","El Salvador"};
+        ArrayAdapter<String> adpSpinner = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, opciones);
+        spPaises.setAdapter(adpSpinner);
+
+
     }
 }
